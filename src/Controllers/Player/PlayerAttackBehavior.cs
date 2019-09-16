ab8ec9ebc039db44b8b52910b33ef433a57a8082
Michael O'Connell <michaeljoconnell@ufl.edu>
2019-09-16 00:31:00 -0400

    cleanup

--- src/Controllers/Player/PlayerAttackBehavior.cs
+++ src/Controllers/Player/PlayerAttackBehavior.cs
@@ -23,22 +23,4 @@ public class PlayerAttackBehavior : StateMachineBehaviour
     {
         animator.SetBool("isAttacking", false); // set to false to exit this state 
     }
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
--- src/Controllers/Player/PlayerJumpBehaviour.cs
+++ src/Controllers/Player/PlayerJumpBehaviour.cs
@@ -59,22 +59,4 @@ public class PlayerJumpBehaviour : StateMachineBehaviour
 
         playerTransform.Translate(inputVector * scaledSpeed); // moves the transform in the direction set above
     }
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
