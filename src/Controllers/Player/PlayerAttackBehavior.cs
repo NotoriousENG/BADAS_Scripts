597c90794de33a87b9aab3d87e843e4216836045
Christina Salem <michaeljoconnell@ufl.edu>
2019-10-01 13:03:51 -0400

    we attackin bb

--- src/Controllers/Player/PlayerAttackBehavior.cs
+++ src/Controllers/Player/PlayerAttackBehavior.cs
@@ -10,13 +10,34 @@ public class PlayerAttackBehavior : StateMachineBehaviour
     {
         audio = animator.gameObject.GetComponent<AudioSource>();
         audio.Play(); // play a sound to signify attacks
-        
-        
+        getAttackAnimator(animator).SetBool("isPlaying", true);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.SetBool("isAttacking", false); // set to false to exit this state 
+        getAttackAnimator(animator).SetBool("isPlaying", false);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        getAttackAnimator(animator).SetBool("isPlaying", false);
+    }
+
+    private Animator getAttackAnimator(Animator animator)
+    {
+        Transform hand = animator.gameObject.transform.Find("Hand");
+        for (int i = 0; i < hand.childCount; i++)
+        {
+
+            GameObject child = hand.GetChild(i).gameObject;
+            if (child.tag.Equals("Weapon"))
+            {
+                Animator childAnim = child.GetComponent<Animator>();
+                return childAnim;
+            }
+        }
+        return null;
     }
 }
